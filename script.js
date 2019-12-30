eea9200a3c99f24d741194f098f522b60edd902b
Your Brennan Predmore <brennnann@gmail.com>
2019-12-29 18:27:34 -0600

    Alert, prompt, and confirm messages in place.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var uppercaseLetters = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z']
 var lowercaseLetters = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z']
 var specialCharacters = ['`,~,!,@,#,$,%,^,&,*,(,)']
+var numericCharacters = ['0','1','2','3','4','5','6','7','8','9']
+
+alert("Your password must be between 8-128 characters!");
+prompt("How long will your password be?");
+alert("You must choose atleast one of the following: upper case, lower case, special, and or numeric characters.");
+alert("We highly recommend chosing as many as possible to increase your password strengh!")
+
+
+confirm("Would you like to use upper case characters?");
+confirm("Would you like to use lower case characters?");
+confirm("Would you like to use special characters?");
+confirm("Would you like to use numeric characters?")
 
 
 // Assignment Code
