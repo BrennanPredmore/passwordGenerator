3724e516aafc4f237fc01d4b37594f0d025cdbe4
Brennan Predmore <brennnann@gmail.com>
2020-11-08 23:05:04 -0600

    prettier format index.html/script.js // remove unused comments // removed unused copyToClipboard() function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 var upperCaseCharactersArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 var lowerCaseCharactersArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
 var numbersArray = '0123456789'.split('');
@@ -10,84 +10,97 @@ var specialCharacters = false;
 var lowerCase = false;
 
 function promptPassword() {
-  var passwordPrompt = prompt("Your password can be from 8-128 characters. What is your password length?");
+  var passwordPrompt = prompt(
+    'Your password can be from 8-128 characters. What is your password length?'
+  );
 
   function validateLength(password) {
     if (password > 7 && password < 128) {
-      passwordLength = password
-      promptSpecialCharacters()
+      passwordLength = password;
+      promptSpecialCharacters();
     } else {
-      alert('You need a different password length!')
+      alert('You need a different password length!');
     }
   }
-  validateLength(passwordPrompt)
+  validateLength(passwordPrompt);
 }
-//this function can be
+
 function promptSpecialCharacters() {
-  var specialCharactersPref = confirm("Including special characters? Click 'OK' to confirm or 'Cancel' to decline.");
+  var specialCharactersPref = confirm(
+    "Including special characters? Click 'OK' to confirm or 'Cancel' to decline."
+  );
 
   function checkSpecialCharacters(preference) {
     if (specialCharactersPref) {
-      specialCharacters = true
+      specialCharacters = true;
     }
-    //function call to next confirm
-    promptLowerCase()
+    promptLowerCase();
   }
-  checkSpecialCharacters(specialCharactersPref)
+  checkSpecialCharacters(specialCharactersPref);
 }
 
 function promptLowerCase() {
-  //this variable belongs to this function :)
-  var lowerCasePref = confirm("Including lower case letters? Click 'OK' to confirm or 'Cancel' to decline.");
+  var lowerCasePref = confirm(
+    "Including lower case letters? Click 'OK' to confirm or 'Cancel' to decline."
+  );
 
   function checkLowerCasePref(preference) {
     if (lowerCasePref) {
-      lowerCase = true
+      lowerCase = true;
     }
-    //function call to next confirm
   }
-  checkLowerCasePref(lowerCasePref)
-  var randomPasswordValue = generateRandomPassword(passwordLength)
-  var passwordArea = document.getElementById('password')
-  passwordArea.innerHTML = randomPasswordValue
+  checkLowerCasePref(lowerCasePref);
+  var randomPasswordValue = generateRandomPassword(passwordLength);
+  var passwordArea = document.getElementById('password');
+  passwordArea.innerHTML = randomPasswordValue;
 }
 
 function generateRandomPassword(passwordLength) {
-  var result = ''
+  var result = '';
   if (!lowerCase && specialCharacters) {
-    allCharacters = [...upperCaseCharactersArray, ...numbersArray, ...specialCharactersArray]
+    allCharacters = [
+      ...upperCaseCharactersArray,
+      ...numbersArray,
+      ...specialCharactersArray,
+    ];
   } else if (!lowerCase && !specialCharacters) {
-    allCharacters = [...upperCaseCharactersArray, ...numbersArray]
+    allCharacters = [...upperCaseCharactersArray, ...numbersArray];
   } else if (lowerCase && !specialCharacters) {
-    allCharacters = [...upperCaseCharactersArray, ...numbersArray, ...lowerCaseCharactersArray]
+    allCharacters = [
+      ...upperCaseCharactersArray,
+      ...numbersArray,
+      ...lowerCaseCharactersArray,
+    ];
   } else {
-    allCharacters = [...upperCaseCharactersArray, ...numbersArray, ...lowerCaseCharactersArray, ...specialCharactersArray]
+    allCharacters = [
+      ...upperCaseCharactersArray,
+      ...numbersArray,
+      ...lowerCaseCharactersArray,
+      ...specialCharactersArray,
+    ];
   }
   for (var i = 0; i < passwordLength; i++) {
-    var allCharsString = allCharacters.join('')
-    result += allCharsString.charAt(Math.floor(Math.random() * allCharacters.length));
+    var allCharsString = allCharacters.join('');
+    result += allCharsString.charAt(
+      Math.floor(Math.random() * allCharacters.length)
+    );
   }
-  return result
+  return result;
 }
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
   passwordText.value = password;
-  copyBtn.removeAttribute("disabled");
+  copyBtn.removeAttribute('disabled');
   copyBtn.focus();
 }
 
-function copyToClipboard() {
-  // BONUS
-}
-// When generate button is pressed, I want the prompt to show up to ask user how many characters they want//
-// Add event listener to generate button
-document.addEventListener("click", event => {
+document.addEventListener('click', (event) => {
   if (!event.target.matches('#generate')) {
     return;
   }
   if (event.target.matches('#generate')) {
-    promptPassword()
+    promptPassword();
   }
-})
+});
