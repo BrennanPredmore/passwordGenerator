d1e7aa8c97338cae7b5b17022469074b18a68e00
Brennan Predmore <brennnann@gmail.com>
2020-11-08 23:26:13 -0600

    number function & uppercase function INIT

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ var passwordLength = 0;
 var specialCharacters = false;
 var lowerCase = false;
 
+
+// VALIDATE PASSWORD LENGTH
 function promptPassword() {
   var passwordPrompt = prompt(
     'Your password can be from 8-128 characters. What is your password length?'
   );
 
   function validateLength(password) {
-    if (password > 7 && password < 128) {
+    if (password > 7 && password < 129) {
       passwordLength = password;
       promptSpecialCharacters();
     } else {
@@ -25,12 +27,13 @@ function promptPassword() {
   validateLength(passwordPrompt);
 }
 
+// SPECIAL CHARACTERS FUNCTION
 function promptSpecialCharacters() {
   var specialCharactersPref = confirm(
     "Including special characters? Click 'OK' to confirm or 'Cancel' to decline."
   );
 
-  function checkSpecialCharacters(preference) {
+  function checkSpecialCharacters() {
     if (specialCharactersPref) {
       specialCharacters = true;
     }
@@ -39,21 +42,68 @@ function promptSpecialCharacters() {
   checkSpecialCharacters(specialCharactersPref);
 }
 
+
+// LOWER CASE FUNCTION
 function promptLowerCase() {
   var lowerCasePref = confirm(
     "Including lower case letters? Click 'OK' to confirm or 'Cancel' to decline."
   );
 
-  function checkLowerCasePref(preference) {
+  function checkLowerCasePref() {
     if (lowerCasePref) {
       lowerCase = true;
     }
   }
+
+  promptUpperCase()
+
   checkLowerCasePref(lowerCasePref);
   var randomPasswordValue = generateRandomPassword(passwordLength);
   var passwordArea = document.getElementById('password');
   passwordArea.innerHTML = randomPasswordValue;
 }
+
+
+// UPPER CASE FUNCTION
+function promptUpperCase() {
+  var upperCasePref = confirm(
+    "Including upper case letters? Click 'OK' to confirm or 'Cancel' to decline."
+  );
+
+  function checkUpperCasePref() {
+    if (upperCasePref) {
+      upperCase = true;
+    }
+  }
+
+  promptNumber()
+  
+  checkUpperCasePref(upperCasePref);
+  var randomPasswordValue = generateRandomPassword(passwordLength);
+  var passwordArea = document.getElementById('password');
+  passwordArea.innerHTML = randomPasswordValue;
+}
+
+
+// NUMBER FUNCTION
+function promptNumber() {
+  var numberPref = confirm(
+    "Including numbers? Click 'OK' to confirm or 'Cancel' to decline."
+  );
+
+  function checkNumberPref() {
+    if (numberPref) {
+      number = true;
+    }
+  }
+  checkNumberPref(numberPref);
+  var randomPasswordValue = generateRandomPassword(passwordLength);
+  var passwordArea = document.getElementById('password');
+  passwordArea.innerHTML = randomPasswordValue;
+}
+
+
+
 
 function generateRandomPassword(passwordLength) {
   var result = '';
